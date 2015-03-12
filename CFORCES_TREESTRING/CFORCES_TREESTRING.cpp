57933d4d7e6f66b4abd8a008e17cdff210a62098
Kareem <lkareem.alil@gmail.com>
2015-03-12 23:47:10 +0200

    now it is accepted

--- CFORCES_TREESTRING/CFORCES_TREESTRING.cpp
+++ CFORCES_TREESTRING/CFORCES_TREESTRING.cpp
@@ -4,28 +4,28 @@
 #include <set>
 using namespace std;
 
-#define MAX 100000
-
 string K; //pattern
 vector<int> T;
 int sp = 0;
 int kp = 0;
+int size_soFar = 0;
 string S;
-int n;
+
 int answer = 0;
 struct Node{
+    int name;
     string s;
     vector<int> sub_nodes;
 };
 
-
-vector<Node> nodes(MAX);
+int MAX = 100001;
+vector<Node> nodes(100001);
 
 
 
 // K is the pattern and T is the failure array
 void preKMP(string K, vector<int> &T){
-    //vector<int> T(K.size() +1 ,-1);
+    
     if(K.size() == 0){
         return;
     }
@@ -38,31 +38,32 @@ void preKMP(string K, vector<int> &T){
     return;
 }
 
-
 //S is the string so far, K is the pattern string
 void KMPdfs(int x, vector<int> &T,int sp,int kp){
-    S += nodes[x].s;
     int matches = 0;
-    while(sp < S.size())
+    int last_size = size_soFar;
+    size_soFar += nodes[x].s.size();
+    while(sp < size_soFar)
     {
-        while(kp != -1 && (kp == K.size() || K[kp] != S[sp])) kp = T[kp];
+        while(kp != -1 && (kp == K.size() || K[kp] != nodes[x].s[sp-last_size])) kp = T[kp];
         kp++;
         sp++;
         if(kp == K.size()) ++matches;
+
     }
     answer += matches;
+    
     for (int i = 0; i < nodes[x].sub_nodes.size(); ++i)
     {
         KMPdfs(nodes[x].sub_nodes[i],T,sp ,kp);
-    }if(n != MAX){
-        S = S.substr(0,S.size()-nodes[x].s.size());
     }
+    size_soFar -=nodes[x].s.size();
  }
 int main()
 {
     cin.sync_with_stdio(false);
 
-    //int n;
+    int n;
     cin >> n;
     Node root;
     root.s = "";
